c01d371feb02bd2859e76b3518882a6d335b24dc
Mike Mulchrone <mtmulch0191@outlook.com>
2024-11-02 23:39:02 -0400

    chat channel list basics

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MaterialModule } from '../../shared/material.module';
+import { ChatChannel } from './types/chat-channel';
 
 @Component({
   selector: 'app-chat',
@@ -9,9 +10,28 @@ import { MaterialModule } from '../../shared/material.module';
   styleUrl: './chat.component.scss',
 })
 export class ChatComponent implements OnInit {
+  
+  public chatChannels: ChatChannel[] = [
+    {
+      id: "0",
+      name: "First Channel"
+    },
+    {
+      id: "1",
+      name: "Second Channel"
+    }
+  ];
+
   ngOnInit(): void {}
 
   public addNewChat(): void {
-    
+    let newChannel = new ChatChannel("2", "Third Channel");
+    this.chatChannels.push(newChannel);
+  }
+
+  public deleteChannel(channel: ChatChannel): void {
+    // TODO make api call to delete channel.
+    // success after api call
+    this.chatChannels = this.chatChannels.filter(x => x.id !== channel.id);
   }
 }
